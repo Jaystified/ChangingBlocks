61fd3217f1e28fdad0ea9a75a8ef6558e07f2d8e
yuji <yuji38kwmt@yahoo.co.jp>
2019-08-20 16:17:59 +0900

    操作ガイドに矢印追加

--- ui.gd
+++ ui.gd
@@ -19,18 +19,36 @@ func get_vector_form_array(arrow):
          return Vector2(1, 0)
         
     return Vector2(0,0)
+        
+
+func get_arrow_texture(arrow):
+    if arrow == UP:
+        return load("res://up-arrow.png") 
+    elif arrow == DOWN:
+         return load("res://down-arrow.png") 
+    elif arrow == LEFT:
+         return load("res://left-arrow.png") 
+    elif arrow == RIGHT:
+         return load("res://right-arrow.png") 
 
 
-func asign_array_block(first_arrow, moving_block):
+func asign_array_block(first_arrow, arrow_tex, moving_block):
     var block_vector = get_vector_form_array(first_arrow)
     var current_position = block_vector * block_size * 2
+    
     
     for arrow in moving_block:
         var arrow_block_scene = load("res://array-block.tscn")
         var scene_instance = arrow_block_scene.instance()
-#        arrow_block_scene.set_name("test")
         scene_instance.position = current_position
         add_child(scene_instance)
+
+        var arrow_block_scene2 = load("res://array-block.tscn")
+        var scene_instance2 = arrow_block_scene.instance()
+        scene_instance2.position = current_position
+        scene_instance2.texture = arrow_tex
+        scene_instance2.z_index = 1
+        add_child(scene_instance2)
         
         current_position += block_vector * block_size
 
@@ -40,10 +58,11 @@ func _ready():
     pass
     
 func update_ui(moving_blocks):    
+
     print(moving_blocks)
-    for first_array in moving_blocks:
-        
-        asign_array_block(first_array, moving_blocks[first_array])
+    for first_arrow in moving_blocks:
+        var arrow_texture = get_arrow_texture(first_arrow)
+        asign_array_block(first_arrow, arrow_texture, moving_blocks[first_arrow])
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
